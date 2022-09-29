66859f7556008a36fe6c4f4e0c5ac51543cabbd2
Max Jockesi Barbosa Soares <99187584+MaxJockesi@users.noreply.github.com>
2022-09-29 14:06:26 -0300

    DEV
    Primeira parte da extração de dados pro DataFrame concluido
    
    Co-Authored-By: FernandoDeMagalhaes <114614766+FernandoDeMagalhaes@users.noreply.github.com>
    Co-Authored-By: Miguel Félix Mediano Lins <99187899+Miguel8219@users.noreply.github.com>

--- source/dados_supercombo.py
+++ source/dados_supercombo.py
@@ -18,21 +18,28 @@ driver.get('https://www.vagalume.com.br/supercombo/discografia/')
 #Extração propriamente dita
 content = driver.page_source
 soup = BeautifulSoup(content)
+
+#Extrai o nome dos álbums e cria uma lista com eles
 for a in soup.find_all('div', attrs={'class':'topLetrasWrapper'}):
     name_album = a.find('h3', attrs={'class':'albumTitle'}).get_text(strip=True)
     albums.append(name_album)
 
+#Extrai o nome das músicas e cria uma lista de listas com elas
 for b in soup.find_all('ol', attrs = {'id':'topMusicList'}):
+    musics_clear = []
     name_musics = b.find_all('a', href = True, attrs={'class':'nameMusic'})
-    musics.append(name_musics)
+    
+    for b1 in name_musics:
+        b1 = b1.string.extract()
+        musics_clear.append(b1)
+        
+    musics.append(musics_clear)
 
+#Extrai o ano dos álbums e cria uma lista com eles
 for c in soup.find_all('div', attrs = {'class': 'cardAlbumInfos'}): 
     year = c.find('p', attrs={'class':'albumYear'}).get_text(strip=True)
-    yearlaunching.append(year)
+    yearlaunching.append(year)   
 
-print(albums, "a")  
-print(musics, "a")
-print(yearlaunching, "a")    
-
+#Criação do DataFrame propriamente dito 
 df = pd.DataFrame({'Album': albums, 'Music': musics, 'Year': yearlaunching})
 df.to_csv('supercombo.csv', index=False, encoding='utf-8')    
