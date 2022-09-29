f999009b92273e25dba7a564050e063251586d5b
Max Jockesi Barbosa Soares <99187584+MaxJockesi@users.noreply.github.com>
2022-09-28 22:14:32 -0300

    DEV
    Programa do DataFrame - Passo inicial
    
    Co-Authored-By: FernandoDeMagalhaes <114614766+FernandoDeMagalhaes@users.noreply.github.com>

--- source/dados_supercombo.py
+++ source/dados_supercombo.py
@@ -11,19 +11,31 @@ albums = [] #Consegue o nome dos álbuns para a música
 musics = [] #Consegue o nome das músicas para os álbuns
 yearlaunching = [] #Consegue o ano de lançamento de certo álbum
 
+
 #Acesso ao site
-driver.get("https://www.vagalume.com.br/supercombo/discografia/")
+driver.get('https://www.vagalume.com.br/supercombo/discografia/')
 
 #Extração propriamente dita
 content = driver.page_source
 soup = BeautifulSoup(content)
-for a in soup.findAll('a',href=True, attrs={'class':'letrasWrapper col1-2'}):
-    name_album = a.find('h3', attrs={'class':'albumTitle'})
-    name_musics = a.find('div', attrs={'class':'nameMusic'})
-    year = a.find('p', attrs={'class':'AlbumYear'})
-    albums.append(name_album.text)
-    musics.append(name_musics.text)
-    yearlaunching.append(year.text)
-print(yearlaunching, "a")    
-df = pd.DataFrame({'Album': albums, 'Music': musics, 'Year': yearlaunching})
-df.to_csv('supercombo.csv', index=False, encoding='utf-8')    
+for a in soup.find_all('div', attrs={'class':'topLetrasWrapper'}):
+    
+    musics_album = []
+    
+    name_album = a.find('h3', attrs={'class':'albumTitle'}).get_text(strip=True)
+    for b in soup.find_all('div', attrs = {'class': 'lineColLeft'}): 
+        name_musics = b.find('a', href = True, attrs={'class':'nameMusic'}).get_text(strip=True)
+        musics_album.append(name_musics)
+    # year = a.find('p', attrs={'class':'AlbumYear'}).get_text(strip=True)
+    musics.append(musics_album)
+    albums.append(name_album)
+    
+    # yearlaunching.append(year)
+    # print(name_album)
+# print(yearlaunching, "a")
+print(albums, "a")  
+print(musics, "a")
+# print(yearlaunching, "a")    
+
+# df = pd.DataFrame({'Album': albums, 'Music': musics, 'Year': yearlaunching})
+# df.to_csv('supercombo.csv', index=False, encoding='utf-8')    
