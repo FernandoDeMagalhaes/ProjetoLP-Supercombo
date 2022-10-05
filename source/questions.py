1cf57fdc1d31d13c16f942facb7b1ba96f80465a
Fernando de Magalhães Toledo <114614766+FernandoDeMagalhaes@users.noreply.github.com>
2022-10-04 21:24:13 -0300

    Finalizado todas as questões
    Todas as questões foram respondidas com sucesso e estão prontas para visualização usando pandas
    
    Co-Authored-By: Max Jockesi Barbosa Soares <99187584+MaxJockesi@users.noreply.github.com>
    Co-Authored-By: Miguel Félix Mediano Lins <99187899+Miguel8219@users.noreply.github.com>

--- source/questions.py
+++ source/questions.py
@@ -1,8 +1,8 @@
-
 import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
-#from PIL import image
+import math
+import sys
 
 # Função pra tirar texto entre parênteses
 def ignorar_caracteres_cercados(texto, char_abertura, char_fechamento):
@@ -24,9 +24,16 @@ def ignorar_caracteres_cercados(texto, char_abertura, char_fechamento):
 
     return novo_texto
 
-#Leitura de arquivo (deve ser necessário ver se funciona em outro PC)
-df1 = pd.read_csv('..\\dataframes\\supercombo.csv')
-df2 = pd.read_csv('..\\dataframes\\supercombo2.csv')
+#Leitura de arquivo
+try:
+    df1 = pd.read_csv('..\\dataframes\\supercombo.csv')
+    df2 = pd.read_csv('..\\dataframes\\supercombo2.csv')
+except FileNotFoundError as fnfe:
+    print(dir(fnfe))
+    print("Arquivo não existe!", fnfe)
+    sys.exit()
+else:
+    print("Nenhum erro ocorreu, arquivo encontrado!")
 
 
 # CONJUNTO DE PERGUNTAS 1
@@ -92,7 +99,7 @@ for i in df1['Album']:
     n2 += 1
 
 
-print("--------------------------------------------------------")
+print("----" * 10)
 
 # Mais ouvidas e menos ouvidas por álbum:
 
@@ -153,7 +160,8 @@ for i in df_dur_album['Duração']:
     n3 += 1
     
 print("\n")
-    
+
+print("----" * 10)
 
 
 # Mais vista e menos vista de todas
@@ -168,7 +176,7 @@ for i in df_viw_album['Visualisações']:
 
 print("\n")
 
-
+print("----" * 10)
 
 # Album mais premiado
 
@@ -180,6 +188,36 @@ for i in df1['Awards']:
 
 print("\n")
 
+print("----" * 10)
+
+# Existe relação entre a duração e a popularidade?
+
+mediaViw = df_viw_album['Visualisações'].mean()
+desvpadViw = df_viw_album['Visualisações'].std()
+
+mediaTempo = df_dur_album['Duração'].mean()
+desvpadTempo = df_dur_album['Duração'].std()
+
+somatorio_xy = 0
+somatorio_x = 0
+somatorio_y = 0
+
+n4 = 0
+for i in df_dur_album['Album']:
+    soma1 = df_viw_album['Visualisações'][n4] - mediaViw
+    soma2 = df_dur_album['Duração'][n4] - mediaTempo
+    soma3 = soma1 * soma2
+    
+    somatorio_xy += soma3
+    somatorio_x += (soma1 ** 2)
+    somatorio_y += (soma2 ** 2)
+    n4 += 1
+
+r = somatorio_xy / math.sqrt(somatorio_x * somatorio_y)
+    
+print("O coeficiente de relação é: ", r , " Portanto não são relacionados \n")
+
+print("----" * 10)
 
 # CONJUNTO DE PERGUNTAS 2
 
@@ -216,6 +254,7 @@ print(df_contagem_album.sort_values(by = 'Contagem', ascending = False).head(3))
 
 print("\n")
 
+print("----" * 10)
 
 # Palavras mais repetidas nos titulos da músicas
 
@@ -259,6 +298,8 @@ df_contagem_album = pd.DataFrame({"Palavra": uniqWord, "Contagem": contagem})
 print("Número \n", df_contagem_album.sort_values(by = 'Contagem', ascending = False).head(5))
 
 print("\n")
+
+print("----" * 10)
 
 d = {}
 for a, x in df_contagem_album.values:
@@ -326,6 +367,8 @@ df_contagem_letra = pd.DataFrame({"Palavra": uniqWord, "Contagem": contagem})
 print(df_contagem_letra.sort_values(by = 'Contagem', ascending = False).head(10))
 
 print("\n")
+
+print("----" * 10)
 
 #Montagem da wordcloud de palvras de toda a discografia
 
@@ -452,12 +495,220 @@ for i in df1['Album']:
     
     n9 += 1
 
+print("----" * 10)
+
+# Palavras do titulo dos albuns como tema nas músicas
+
+musicas = df2['Music']
+albuns = []
+letras = []
+
+m1 = 0
+for i in musicas:
+    n1 = 0
+    for j in df1['Music']:
+        if i in j:
+            albuns.append(df1['Album'][n1])
+        n1 = n1+1
+    letras.append(df2['Lyrics'][m1])
+    m1 = m1 + 1
+    
+df_lyric_album = pd.DataFrame({'Album': albuns, 'Musica': musicas, 'Letras': letras})
+
+liricList = []
+
+for i in df1['Album']:
+    liric = []
+    m7 = 0
+    for j in df2['Lyrics']:
+        if (i == df_lyric_album['Album'][m7]):
+            liric.append(df_lyric_album['Letras'][m7])
+        m7 = m7+1
+    liricList.append(liric)
+
+stringclearList = []
+albumclearList = []
+
+for j in liricList:
+    stringclear = []
+    for i in j:
+        string = i
+        remove = "[];:!?.,'-"
+        for k in range(len(remove)):
+            string = string.replace(remove[k],"")
+        stringclear.append(string.upper())
+    
+    stringclearList.append(stringclear)
+
+
+albumclear = []
+for j in df1['Album']:
+    string = j
+    remove = "[];:!?.,'-"
+    for k in range(len(remove)):
+        string = string.replace(remove[k],"")
+    albumclear.append(string.upper())
+
+uniqWordList = []
+
+palavras1 = []
+
+for i in stringclearList:
+    bigstring1 = ""
+    
+    for j in i:
+        bigstring1 = bigstring1 + " " + j
+
+    bigstring1 = bigstring1.replace("("," ")
+    bigstring1 = bigstring1.replace(")"," ")
+
+    palavras = bigstring1.split()
+    
+    remover = ['A','E','O','DE','SE','QUE','EU','TE','DO','EM','ME','TA','OS','AS',
+               'É','NÃO','VO','TO','DA','SO','EI','AI','IR','PRA','OU','NO','UM','CÊ',
+               'VOCÊ','NA','HA','IA','COM','VI','Ó','SER','SEM','CON','SI','TU','MEU',
+               'TEM','SÓ','OI','LI','AA','DEI','VER','VAI','PAR','POR','MAIS','FAZ','PRO',
+               'NOS','AH','TER','VOU','ERA','SEI','SOU','DOS','ESTA','DAR','MAS','DEIXA','ESSE',
+               'ELA','QUER','COMO','QUANDO','MINHA','QUEM', 'OH', 'FOR', 'DEVE', 'TÁ', 'AÍ',
+               'TÃO', 'AAAAA', 'MIM', 'WAAAA', 'OOO', 'UOOO', 'AAAAAH', 'JÁ']
+
+    for i in remover:
+        try:
+            while True:
+                palavras.remove(i)
+        except:
+            pass
+    
+    palavras1.append(palavras)
+
+
+palavras2 = []
+
+for i in albumclear:
+
+    i = i.replace("("," ")
+    i = i.replace(")"," ")
+
+    palavras2.append(i.split())
+
+contagemList = []
+
+n11 = 0
+for i in palavras2:
+    contagem = []
+    for l in i:
+        contagem.append(palavras1[n11].count(l))
+
+    contagemList.append(contagem)
+    n11 += 1
+
+
+n12 = 0
+for i in df1['Album']:
+    df_titulo_album_letra = pd.DataFrame({"Palavra": palavras2[n12], "Contagem": contagemList[n12]})
+    
+    print(i, "\n")
+    print(df_titulo_album_letra.sort_values(by = 'Contagem', ascending = False))
+    print("\n")
+    
+    n12 = n12 + 1
+
+print("----" * 10)
+
+# Titulo das músicas são tema recorente nas letras?
+
+stringclear1 = []
+for j in df2['Music']:
+    string = j
+    remove = "[];:!?.,'-"
+    for k in range(len(remove)):
+        string = string.replace(remove[k],"")
+    stringclear1.append(string.upper())
+
+n13 = 0
+for i in stringclear1:
+    stringclear1[n13] = ignorar_caracteres_cercados(stringclear1[n13], '(', ')')
+    
+    stringclear1[n13] = stringclear1[n13].split()
+
+    remover = ['A','O','E','DA','DE','EU','AS','SE','DO','NA']
+
+    for j in remover:
+        try:
+            while True:
+               stringclear1[n13].remove(j)
+        except:
+            pass
+        
+    n13 += 1
+    
+stringclear2 = []
+for j in df2['Lyrics']:
+    string = j
+    remove = "[];:!?.,'-"
+    for k in range(len(remove)):
+        string = string.replace(remove[k],"")
+    stringclear2.append(string.upper())
+
+
+n14 = 0
+for i in stringclear2:
+    
+    stringclear2[n14] = stringclear2[n14].replace("("," ")
+    stringclear2[n14] = stringclear2[n14].replace(")"," ")
+    
+    stringclear2[n14] = stringclear2[n14].split()
+    
+    remover = ['A','E','O','DE','SE','QUE','EU','TE','DO','EM','ME','TA','OS','AS',
+               'É','VO','TO','DA','SO','EI','AI','IR','PRA','OU','NO','UM','CÊ',
+               'VOCÊ','NA','HA','IA','COM','VI','Ó','SER','SEM','CON','SI','TU','MEU',
+               'TEM','SÓ','OI','LI','AA','DEI','VER','VAI','PAR','MAIS','FAZ','PRO',
+               'NOS','AH','TER','VOU','ERA','SEI','SOU','DOS','ESTA','DAR','MAS','DEIXA','ESSE',
+               'QUER','QUANDO','MINHA','QUEM', 'OH', 'FOR', 'DEVE', 'TÁ', 'AÍ',
+               'TÃO', 'AAAAA', 'MIM', 'WAAAA', 'OOO', 'UOOO', 'AAAAAH', 'JÁ']
+
+    for j in remover:
+        try:
+            while True:
+                stringclear2[n14].remove(j)
+        except:
+            pass
+    
+    n14 += 1
+
+contagemList = []
+
+n11 = 0
+for i in stringclear1:
+    contagem = []
+    for l in i:
+        contagem.append(stringclear2[n11].count(l))
+
+    contagemList.append(contagem)
+    n11 += 1
+
+n15 = 0
+for i in df2['Music']:
+    df_titulo_musica_letra = pd.DataFrame({"Palavra": stringclear1[n15], "Contagem": contagemList[n15]})
+    
+    print(i, "\n")
+    print(df_titulo_musica_letra.sort_values(by = 'Contagem', ascending = False))
+    print("\n")
+    
+    n15 = n15 + 1
+
+print("----" * 10)
+
 #Conjunto de perguntas 3 - A primeira pergunta está na parte anterior
 #(Médias do tamanho das músicas por álbum)
 
 #Média do tempo das músicas
 print("Média do tempo das músicas na discografia: ", round(df2['Time'].mean(),2),'\n')
 
+print("----" * 10)
+
 #Gravadora mais comum, com número de vezes
 recorders = df1['Recorders'].value_counts().sort_values(ascending = False).head(1)
 print("Gravadora mais comum e número de vezes: \n ", recorders.to_string())
+
+print("----" * 10)
